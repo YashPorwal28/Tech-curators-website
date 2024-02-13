215419e71cdfb7cf11bed28b000d0de56747e90d
YashPorwal28 <yashporwal321@gmail.com>
2024-02-13 23:47:06 +0530

    fixing footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <div className="bg-[#121619]">
       <div className="w-3/5 m-auto  ">
-        <div className="footer_top flex flex-col gap-6 px-6 py-3 bg-[#0F2127] rounded-lg lg:flex-row  h-[260px] lg:px-[100px] lg:items-center lg:justify-between">
-          <div className="contactUs flex flex-col gap-4 mt-3  w-[500px]  ">
+        <div className="footer_top flex flex-col gap-6 px-6 py-3 bg-[#0F2127] rounded-lg lg:flex-row pb-10 pt-10  lg:px-[100px] lg:items-center lg:justify-between">
+          <div className="contactUs flex flex-col gap-4 mt-3    ">
             <div className="contact_us_box py-1 px-1 rounded-md bg-[#42BE65] w-[90px]">
               <p className="text-sm">Contact us:</p>
             </div>
@@ -28,7 +28,7 @@ const Footer = () => {
         
 
         </div>
-        <div className="right_reserved h-[40px] border-t border-black bg-[#0F2127]">
+        <div className="right_reserved h-[40px] border-t  border-black bg-[#0F2127]">
 
         <p className="text-white px-[30px] mt-3">@ 2024 Techcurators. All Rights Reserved.</p>
         </div>
